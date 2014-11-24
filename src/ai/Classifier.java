f2b2d2d79d4a34754eec8bcc77f256dab9922ec2
Michiel te Braake <m.tebraake.95@gmail.com>
2014-11-24 16:52:37 +0100

    Better classifier

--- src/ai/Classifier.java
+++ src/ai/Classifier.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -23,6 +22,9 @@ public class Classifier {
         trainedF.put("!total", 0);
         train("M", trainedM);
         train("F", trainedF);
+
+        System.out.println("Percentage M correct: " + categorizeAll("M"));
+        System.out.println("Percentage F correct: " + categorizeAll("F"));
     }
 
     private void train(String category, HashMap<String, Integer> trained) {
@@ -32,9 +34,6 @@ public class Classifier {
                 List<String> lines = Files.readAllLines(file.toPath(), Charset.defaultCharset());
                 for (String line : lines) {
                     for (String word : tokenizer.tokenize(line)) {
-                        if (word.equals("")) {
-                            System.out.println(Arrays.toString(tokenizer.tokenize(line)));
-                        }
                         if (trained.containsKey(word)) {
                             trained.put(word, trained.get(word) + 1);
                         } else {
@@ -50,12 +49,54 @@ public class Classifier {
         System.out.println(trained);
     }
 
-    private int categorize() {
-        return 0;
+    private double categorizeAll(String category) {
+        int correct = 0;
+        int total = 0;
+        File folder = new File("blogstest/" + category);
+        for (File file : folder.listFiles()) {
+            if (categorize(file).equals(category)) {
+                correct++;
+            }
+            total++;
+        }
+
+        System.out.println("Correct: " + correct + " Total: " + total);
+
+        return (double) correct / total;
     }
 
-    private int getProbabilty() {
+    private String categorize(File file) {
+        double scoreM = 1;
+        double scoreF = 1;
+        double k = 1;
+        long totalM = trainedM.get("!total");
+        long totalF = trainedF.get("!total");
+        try {
+            List<String> lines = Files.readAllLines(file.toPath(), Charset.defaultCharset());
+            for (String line : lines) {
+                for (String word : tokenizer.tokenize(line)) {
+                    int wordOccurences = 0;
+                    if (trainedM.containsKey(word)) {
+                        wordOccurences = trainedM.get(word);
+                    }
+                    scoreM += Math.log((wordOccurences + k) / (totalM + k * totalM));
 
-        return 0;
+                    wordOccurences = 0;
+                    if (trainedF.containsKey(word)) {
+                        wordOccurences = trainedF.get(word);
+                    }
+                    scoreF += Math.log((wordOccurences + k) / (totalF + k * totalF));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //System.out.println(file.getPath() + ": " + "score m: " + scoreM + ", score f: " + scoreF);
+        double result = scoreM - scoreF;
+        if (result > 0) {
+            return "M";
+        } else {
+            return "F";
+        }
     }
 }
